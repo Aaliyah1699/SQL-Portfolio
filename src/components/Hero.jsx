81ad5505a7151fe15d77fdc70b56fe9c24d2b61d
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2025-04-01 19:10:39 -0500

    Update Hero and fonts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,17 @@ import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 const Hero = () => {
     return (
-        <div className='bg-rose-100 py-8'>
+        <div className='bg-violet-100 py-8'>
             <div className='align-element grid md:grid-cols-2 items-center gap-8'>
                 <article>
-                    <h1 className='text-6xl font-bold tracking-wide font-bruno text-slate-950'>
+                    <h1 className='text-7xl font-bold tracking-wide font-candy text-slate-950'>
                         Aaliyah M.
                     </h1>
-                    <p className='mt-4 text-3xl text-slate-800 capitalize tracking-wide font-jura'>
-                        Software Engineer
+                    <p className='mt-4 text-4xl text-slate-800 capitalize tracking-wide font-one font-semibold'>
+                        Data Analyst
                     </p>
-                    <p className='mt-2 text-lg text-slate-800 capitalize tracking-wide font-jura'>
-                        Creating pathways to a world of boundless opportunities.
+                    <p className='mt-2 text-xl text-slate-800 capitalize tracking-wide font-one font-semibold'>
+                        Unfolding stories woven beneath the surface.
                     </p>
                     <div className='flex gap-x-4 mt-4'>
                         {/* Add links */}
@@ -22,14 +22,14 @@ const Hero = () => {
                             target='_blank'
                             rel='noopener noreferrer'
                         >
-                            <FaGithubSquare className='h-8 w-8 text-rose-600 hover:text-rose-950 duration-300 ' />
+                            <FaGithubSquare className='h-8 w-8 text-violet-600 hover:text-rose-950 duration-300 ' />
                         </a>
                         <a
                             href='https://www.linkedin.com/in/aaliyahmontgomery'
                             target='_blank'
                             rel='noopener noreferrer'
                         >
-                            <FaLinkedin className='h-8 w-8 text-rose-600 hover:text-rose-950 duration-300 ' />
+                            <FaLinkedin className='h-8 w-8 text-violet-600 hover:text-rose-950 duration-300 ' />
                         </a>
                     </div>
                 </article>
