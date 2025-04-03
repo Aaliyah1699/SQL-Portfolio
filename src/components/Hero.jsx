434c5c463802f1fcd998f18510172a4bf30437d5
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2025-04-02 21:01:06 -0500

    Update font, size, & icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,18 @@
 import hero from '/src/Hero.svg';
-import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { FiLinkedin, FiGithub } from 'react-icons/fi';
 
 const Hero = () => {
     return (
         <div className='bg-violet-100 py-8'>
             <div className='align-element grid md:grid-cols-2 items-center gap-8'>
                 <article>
-                    <h1 className='text-7xl font-bold tracking-wide font-candy text-slate-900'>
-                        Aaliyah M.
+                    <h1 className='text-8xl font-bold tracking-wide font-dance text-slate-900'>
+                        Aaliyah <span className='text-violet-600'>M.</span>
                     </h1>
-                    <p className='mt-4 text-4xl text-slate-900 capitalize tracking-wide font-one font-semibold'>
+                    <p className='mt-4 text-4xl text-slate-900 capitalize tracking-wide font-iana font-semibold'>
                         Data Analyst
                     </p>
-                    <p className='mt-2 text-xl text-slate-900 capitalize tracking-wide font-one font-semibold'>
+                    <p className='mt-2 text-xl text-slate-900 capitalize tracking-wide font-iana font-semibold'>
                         Unfolding stories woven beneath the surface.
                     </p>
                     <div className='flex gap-x-4 mt-4'>
@@ -22,14 +22,14 @@ const Hero = () => {
                             target='_blank'
                             rel='noopener noreferrer'
                         >
-                            <FaGithubSquare className='h-8 w-8 text-violet-600 hover:text-rose-950 duration-300 ' />
+                            <FiGithub className='h-8 w-8 text-violet-600 hover:text-rose-950 duration-300 ' />
                         </a>
                         <a
                             href='https://www.linkedin.com/in/aaliyahmontgomery'
                             target='_blank'
                             rel='noopener noreferrer'
                         >
-                            <FaLinkedin className='h-8 w-8 text-violet-600 hover:text-rose-950 duration-300 ' />
+                            <FiLinkedin className='h-8 w-8 text-violet-600 hover:text-rose-950 duration-300 ' />
                         </a>
                     </div>
                 </article>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { links } from '../data';
 
+
 const Navbar = () => {
     return (
-        <nav className='bg-violet-100' >
+        <nav className='bg-violet-100'>
             <div className='align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8 leading-loose '>
-                <h2 className='text-4xl font-extrabold tracking-wide font-candy text-slate-900'>
+                <h2 className='text-5xl font-extrabold tracking-wide font-dance text-slate-900'>
                     Analytix<span className='text-violet-600'>Hub</span>
                 </h2>
                 <div className='flex gap-x-3 leading-loose border-b border-slate-900 '>
@@ -15,7 +16,7 @@ const Navbar = () => {
                                 key={id}
                                 href={href}
                                 to={to}
-                                className='capitalize text-lg tracking-widest hover:text-violet-600 duration-300 font-one hover:shadow-lg text-slate-900'
+                                className='capitalize text-lg tracking-widest hover:text-violet-600 duration-300 font-iana hover:shadow-lg text-slate-900'
                             >
                                 {text}
                             </a>
