5c11d14da237db73fea69d5f3f7d66dad64cca76
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2025-04-03 13:04:18 -0500

    Add icon to section titles

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,11 +1,27 @@
+import { BsBalloonHeart } from 'react-icons/bs';
+
 const SectionTitle = ({ text }) => {
     return (
         <div className='border-b border-gray-200 pb-5'>
-            <h2 className='text-5xl font-medium tracking-wider capitalize font-dance'>
+            <h2 className='flex items-center text-5xl font-medium tracking-wider capitalize font-dance'>
                 {text}
+                <BsBalloonHeart className='ml-2  text-violet-500 h-10 w-10 pt-1' />
             </h2>
         </div>
     );
 };
 
+
 export default SectionTitle;
+
+
+//  No icon code
+// const SectionTitle = ({ text }) => {
+//     return (
+//         <div className='border-b border-gray-200 pb-5'>
+//             <h2 className='text-5xl font-medium tracking-wider capitalize font-dance'>
+//                 {text} <BsBalloonHeart />
+//             </h2>
+//         </div>
+//     );
+// };
