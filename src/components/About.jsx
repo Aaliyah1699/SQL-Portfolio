02a3bb163f6333eb21c4c10654dd1f5bf07482e8
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2025-04-03 12:07:14 -0500

    Update about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,21 +3,22 @@ import SectionTitle from './SectionTitle';
 
 const About = () => {
     return (
-        <section className='bg-white py-20 font-one font-bold' id='about'>
+        <section className='bg-white py-20 ' id='about'>
             <div className='align-element grid md:grid-cols-2 items-center gap-16'>
                 <img src={about} alt='undraw image' className='w-full h-64' />
                 <article>
                     <SectionTitle text='About Me' />
-                    <p className='text-slate-900 mt-8 leading-loose text-lg'>
-                        I'm Aaliyah Montgomery, a data analyst with a strong
-                        foundation in Python, SQL, and data visualization. I
-                        enjoy transforming raw data into meaningful insights
-                        that drive informed decision-making. With a passion for
-                        problem-solving and continuous learning, I thrive on
-                        uncovering patterns, optimizing processes, and
-                        presenting data in clear, impactful ways. My goal is to
-                        leverage data to guide strategic decisions and create
-                        actionable solutions.
+                    <p className='text-slate-900 mt-8 leading-loose text-lg font-iana'>
+                        I'm Aaliyah Montgomery, a dedicated data analyst
+                        specializing in Python, SQL, and data visualization. I
+                        transform raw data into powerful insights that inform
+                        strategic decisions and drive business growth.
+                        Passionate about problem-solving and continuous
+                        learning, I excel at uncovering hidden patterns,
+                        optimizing processes, and crafting clear, impactful
+                        visual narratives. My mission is to leverage data to
+                        unlock actionable solutions and guide strategic
+                        decision-making.
                     </p>
                 </article>
             </div>
